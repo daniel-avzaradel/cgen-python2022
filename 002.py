4d7ee4ae72fbfe95b449650ea47d7fe00b7ff942
Daniel Avzaradel <daniela@cgen.com>
2022-03-29 13:33:46 +0300

    second class, python compugen

---
+++ 002.py
@@ -0,0 +1,20 @@
+# X if 
+
+# X and Y
+
+
+# X or Y
+temp = 'gaga' or 100
+print(temp)
+
+# not X
+temp = not []
+print(temp)
+
+# X in Y, X not in Y
+numbers = [12, 4, 65]
+for number in numbers:
+    print(number)
+
+if 12 in numbers:
+    print('Yes, 12 is in numbers...')
