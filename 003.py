8003c8f2bdd417453197d5dc73ec9f238e6dff4a
Daniel Avzaradel <daniela@cgen.com>
2022-04-05 15:36:49 +0300

    nested functions, legb rules

--- 003.py
+++ 003.py
@@ -8,3 +8,36 @@ ob = [12, 4, 76]
 f(ob)
 print(ob)
 
+sales = 5000
+income = 1000
+
+if sales>1000:
+    def bonus(income):
+        return income * 0.1
+else:
+    income *= 0.05
+
+print(income)
+
+# nonlocal
+# defines a variable not to be on local scope
+
+def a():
+    temp = 12
+    def b():
+        nonlocal temp
+        temp = 4
+    b()
+    print(temp)
+    return temp
+a()
+
+def doSomething():
+    global temp
+    temp = 99
+    print(temp)
+
+doSomething()
+
+
+## nested functions
