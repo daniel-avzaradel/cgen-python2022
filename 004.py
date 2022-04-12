a6e2640ef7398be77ded28e26d7272847cba9177
Daniel Avzaradel <daniela@cgen.com>
2022-04-12 10:29:42 +0300

    zip function

--- 004.py
+++ 004.py
@@ -1,1 +1,12 @@
 ## 12/04/2022 - 4th meeting
+
+# zip function
+
+ids = [12, 23, 34, 14, 16]
+names = ['moshe', 'daniel', 'laura', 'paola']
+data = zip(ids, names)
+
+print(type(data))
+
+for item in data:
+    print(item)
