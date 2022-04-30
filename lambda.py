e05b06e50c3e9a2e8357c61a7ea5266fd3a0c63e
Daniel Avzaradel <daniela@cgen.com>
2022-05-01 02:10:41 +0300

    exercise

--- lambda.py
+++ lambda.py
@@ -2,6 +2,10 @@ f = lambda a, b=10: a * b
 print(f(2))
 
 ## tuple packing
-
+# built-in function len() = length
 f = lambda *numbers: len(numbers)
 print(f(2, 3, 4, 11, 6))
+
+## exercise
+total = lambda num1, num2: num1 + num2
+print(total(8, 2))
