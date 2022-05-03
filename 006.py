51a37b31e29ef0a01f6390c1afb8faaa2830b8ed
Daniel Avzaradel <daniela@cgen.com>
2022-05-03 11:04:11 +0300

    sets operations

--- 006.py
+++ 006.py
@@ -3,3 +3,15 @@
 
 ob  = set('abcdefg')
 print(ob)
+
+basket_one = {'a', 'b', 'c'}
+basket_two = {'a', 'd', 'g'}
+
+print('a' in basket_one)          # checking the membership
+print(basket_one - basket_two)    # difference between two sets
+print(basket_one | basket_two)    # union of two sets
+print(basket_one & basket_two)    # intersection between two sets
+print(basket_one ^ basket_two)    # difference between two sets
+print(set('bcd') < basket_one)    # subset of another set
+print(set('abcde') > basket_one)  # superset of another set
+
