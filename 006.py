16f65d3181ea22cf8cf8569ee76803330d0299f2
Daniel Avzaradel <daniela@cgen.com>
2022-05-03 12:33:15 +0300

    operators on set ex

--- 006.py
+++ 006.py
@@ -15,3 +15,9 @@ print(basket_one ^ basket_two)    # difference between two sets
 print(set('bcd') < basket_one)    # subset of another set
 print(set('abcde') > basket_one)  # superset of another set
 
+x = {'dave','mike','jane'}        #creating new set
+x.add('ron')                      #insert a new item
+x.update({'shimrit','sandra'})    #merge with other set
+x.remove('dave')                  #remove item
+
+print(x)
