53fcbda9966602d95357b76b5193c73eb0016859
Daniel Avzaradel <daniela@cgen.com>
2022-05-10 10:51:28 +0300

    copy function

---
+++ 007.py
@@ -0,0 +1,22 @@
+## Tuples
+### IMMUTABLE ###
+
+## tuple operations
+
+()                    # creates an empty tuple
+myTuple = (0,)        # creates one item tuple
+myTuple = (1, 2, 3)   # creates three items tuple
+
+
+## copy function
+
+a = [1, 4, 7, 32, 20]
+b = ['a', 'b', 'c', a]
+print(b)
+
+c = b.copy()
+print(c)
+
+a[0] = 999
+print(b)
+print(c)
