4bfb84938ef2f16128f01649b1c72760e740166c
Daniel Avzaradel <daniela@cgen.com>
2022-05-15 08:03:51 +0300

    using statement pass for simple class definition

--- 005.py
+++ 005.py
@@ -9,3 +9,11 @@ class myClass:
 john = myClass()
 john.setData('John')
 john.printName()
+
+## the simplest python class definition
+
+class myNewClass: pass
+ob = myNewClass()
+ob.text = 'abc'
+ob.numbers = 123
+print(ob.text, ob.numbers)
