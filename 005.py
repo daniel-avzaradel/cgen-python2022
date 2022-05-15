8df7e08e011f42b342207e1fa908d78dbf8d25dc
Daniel Avzaradel <daniela@cgen.com>
2022-05-15 08:12:59 +0300

    using the __init__ function to define a class constructor

--- 005.py
+++ 005.py
@@ -17,3 +17,20 @@ ob = myNewClass()
 ob.text = 'abc'
 ob.numbers = 123
 print(ob.text, ob.numbers)
+
+
+## the __init__ function
+## The __init__ method is Python's replacement for the constructor. When we create a new object this function will be invoked. 
+
+class Rectangle:
+  def __init__(self, w, h):
+    self.width = w
+    self.height = h
+  def printDetails(self):
+    print('width:', self.width, 'height:', self.height)
+  def area(self):
+    print('the area of the rectangle is:', self.width * self.height)
+
+newRectangle = Rectangle(12, 20)
+newRectangle.printDetails()
+newRectangle.area()
